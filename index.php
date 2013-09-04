f94d401d70330716b7c2e02f1cbb9ca372548186
Oz Nazilli <oz@nazilli.co>
2013-09-03 20:16:00 -0400

    Bootstrap Installed

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
+</html>
