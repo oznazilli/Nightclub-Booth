ad97dd6ca5447121b9781396ac56d1195ca15844
Oz Nazilli <oz@nazilli.co>
2013-09-12 22:21:56 -0400

    done with the basics, needs some tweaking

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -1,29 +1,25 @@
-<div class="content">
-	<div class="col-lg-12">
-		<header class="clearfix">
-			<section id="branding">
-				<a href="index.php"><img src="img/logo.png" alt="Nightclub Booth Logo"></a>
-			</section><!-- branding -->
+<header class="clearfix">
+	<section id="branding">
+		<a href="index.php"><img src="img/logo.png" alt="Nightclub Booth Logo"></a>
+	</section><!-- branding -->
 
-			<section class="navbar navbar-inverse navbar-default">
-				<ul class="nav navbar-nav">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="features.php">Features</a></li>
-					<li><a href="pricing.php">Pricing</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-							<li><a tabindex="-1" href="aboutus.php">Meet the Team</a></li>
-							<li><a tabindex="-1" href="backers.php">Backers</a></li>
-						</ul> <!-- dropdown menu -->
-					</li>
-					<li><a href="customers.php">Customers</a></li>
-					<li><a href="contactus.php">Contact Us</a></li>
-				</ul><!-- nav -->
-				<ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Sign In</a></li></ul>
-			</section><!-- navbar -->
+	<section class="navbar navbar-inverse navbar-default">
+		<ul class="nav navbar-nav">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="features.php">Features</a></li>
+			<li><a href="pricing.php">Pricing</a></li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>
+				<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+					<li><a tabindex="-1" href="aboutus.php">Meet the Team</a></li>
+					<li><a tabindex="-1" href="backers.php">Backers</a></li>
+				</ul> <!-- dropdown menu -->
+			</li>
+			<li><a href="customers.php">Customers</a></li>
+			<li><a href="contactus.php">Contact Us</a></li>
+		</ul><!-- nav -->
+		<ul class="nav navbar-nav navbar-right">
+<li><a href="#">Sign In</a></li></ul>
+	</section><!-- navbar -->
 
-		</header>
-	</div><!-- column -->
-</div><!-- content -->
+</header>
